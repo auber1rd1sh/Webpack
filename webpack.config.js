eeb3df584267d639eb920462969eec2294c8bef5
auber1rd1sh <kristinina333@gmail.com>
2024-12-13 17:10:03 +0300

    Первое задание реализовано

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.js', 
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'), 
+    clean: true, 
+  },
+  mode: 'development', 
+  module: {
+    rules: [
+      {
+        test: /\.css$/, 
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html', 
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css', 
+    }),
+  ],
+  devServer: {
+    static: './dist', 
+    port: 3000, 
+    open: true, 
+  },
+};
